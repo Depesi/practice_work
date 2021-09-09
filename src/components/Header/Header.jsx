c6f064d370df5f7b21a04125d761e434d0c6a65e
Depesi <anton.tsar02@gmail.com>
2021-09-09 10:32:59 +0300

    createToggle

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,16 +18,36 @@ const Header = (props) => {
 						<ul className={style.header__navbar}>
 							<li><NavLink to={"/main"}> Main</NavLink></li>
 							<li><NavLink to={"/contacts"}> Contacts</NavLink></li>
-							{theme.darkMode
+
+							<div className={style.toggleContainer}
+								onClick={theme.darkMode ? () => props.setThemeMode(false) : () => props.setThemeMode(true)}>
+								<div className={style.toggleWrapper}>
+									<input type={'checkbox'} className={style.dn} id="dn" checked={theme.darkMode ? true : false} />
+									<label for="dn" className={style.toggle}>
+										<span className={style.toggle__handler}>
+											<span className={style.crater + ' ' + style.crater1}></span>
+											<span className={style.crater + ' ' + style.crater2}></span>
+											<span className={style.crater + ' ' + style.crater3}></span>
+										</span>
+										<span className={style.star + ' ' + style.star1}></span>
+										<span className={style.star + ' ' + style.star2}></span>
+										<span className={style.star + ' ' + style.star3}></span>
+										<span className={style.star + ' ' + style.star4}></span>
+										<span className={style.star + ' ' + style.star5}></span>
+										<span className={style.star + ' ' + style.star6}></span>
+									</label>
+								</div>
+							</div>
+							{/* {theme.darkMode
 								? <img src={darkTheme} className={style.swap__theme_icon}
 									onClick={() => { props.setThemeMode(false) }} />
 								: <img src={lightTheme} className={style.swap__theme_icon}
-									onClick={() => { props.setThemeMode(true) }} />}
+									onClick={() => { props.setThemeMode(true) }} />} */}
 						</ul>
 					</nav>
 				</div>
 			</div>
-		</header>
+		</header >
 	)
 }
 
